66626d765da0ad3674a12428b23a2be2542f64f1
Matheus <matheushenrry00@gmail.com>
2026-03-15 21:41:38 -0300

    Projeto inicial do flask-workflow

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sistema de Biblioteca"
+
+@app.route("/sobre")
+def sobre():
+    return "Projeto da disciplina de CI/CD"
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
