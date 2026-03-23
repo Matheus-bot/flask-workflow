adab3708b4e6c3bcf38feb07f18468e423fa91c8
MATHEUS HENRIQUE DE CAMPOS RUMAO <1051392421015@fatec.corp>
2026-03-23 08:42:18 -0300

    ci: adiciona pipeline estruturada

---
+++ flask-workflow-main/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sistema de Gerenciamento de Biblioteca"
+
+@app.route("/sobre")
+def sobre():
+    return "Projeto da disciplina de CI/CD"
+
+@app.route("/livros")
+def livros():
+    return "Lista completa de livros da biblioteca"
+
+@app.route("/autores")
+def autores():
+    return "Lista completa de autores"
+
+@app.route("/contato")
+def contato():
+    return "Página de contato"
+
+
+@app.route("/cadastro-livro")
+def cadastro_livro():
+    return "Formulário de cadastro de livros"
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
